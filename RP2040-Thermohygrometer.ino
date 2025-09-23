d8a27d2c419d0f2d6c6cb983fd9a72b02c604843
Bread Board Maniax <bbmaniax@users.noreply.github.com>
2025-09-23 11:31:16 +0900

    refactor: main

--- RP2040-Thermohygrometer.ino
+++ RP2040-Thermohygrometer.ino
@@ -17,28 +17,16 @@
 #include "View.h"
 
 #define BUTTON1_GND_PIN 28
-
 #define BUTTON1_INPUT_PIN 26
 #define BUTTON2_INPUT_PIN 29
-
+#define SENSOR_READ_INTERVAL_MS 3000
 #define DISPLAY_I2C_ADDRESS 0x3C
 #define DISPLAY_WIDTH 128
 #define DISPLAY_HEIGHT 64
 
 #define HISTORY_BUFFER_SIZE (DISPLAY_WIDTH / (PLOT_HORIZONTAL_SPACING + 1) + PLOT_HORIZONTAL_SPACING)
-
-#define SENSOR_READ_INTERVAL_MS 3000
 #define PLOT_HORIZONTAL_SPACING 1
 
-GND gnd1(BUTTON1_GND_PIN);
-Button button1(BUTTON1_INPUT_PIN);
-Button button2(BUTTON2_INPUT_PIN);
-Adafruit_AHTX0 thermometer;
-Adafruit_BMP280 barometer;
-Adafruit_SSD1306 display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
-TimeKeeper timeKeeper1(SENSOR_READ_INTERVAL_MS);
-
-SensorManager::SensorData sensorData;
 int16_t temperatureHistoryBuffer[HISTORY_BUFFER_SIZE];
 int16_t humidityHistoryBuffer[HISTORY_BUFFER_SIZE];
 int16_t pressureHistoryBuffer[HISTORY_BUFFER_SIZE];
@@ -46,11 +34,19 @@ History temperatureHistory(temperatureHistoryBuffer, HISTORY_BUFFER_SIZE);
 History humidityHistory(humidityHistoryBuffer, HISTORY_BUFFER_SIZE);
 History pressureHistory(pressureHistoryBuffer, HISTORY_BUFFER_SIZE);
 
+GND gnd1(BUTTON1_GND_PIN);
+Button button1(BUTTON1_INPUT_PIN);
+Button button2(BUTTON2_INPUT_PIN);
+TimeKeeper timeKeeper1(SENSOR_READ_INTERVAL_MS);
+Adafruit_AHTX0 thermometer;
+Adafruit_BMP280 barometer;
+Adafruit_SSD1306 display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
 EventManager eventManager(button1, button2, timeKeeper1);
 SensorManager sensorManager(thermometer, barometer);
 
 Model model(temperatureHistory, humidityHistory, pressureHistory);
-View view(model, display, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+View view(model,display, DISPLAY_WIDTH, DISPLAY_HEIGHT);
 
 void setup() {
   DEBUG_SERIAL_BEGIN(9600);
@@ -60,13 +56,13 @@ void setup() {
   DEBUG_SERIAL_PRINTLN("Thermohygrometer");
 
   gnd1.begin();
-
   eventManager.begin();
   sensorManager.begin();
 
+  SensorManager::SensorData sensorData;
   sensorManager.readSensorData(&sensorData);
-  model.begin(sensorData.temperature, sensorData.humidity, sensorData.pressure);
 
+  model.begin(sensorData.temperature, sensorData.humidity, sensorData.pressure);
   view.begin(DISPLAY_I2C_ADDRESS);
   delay(1000);
 
@@ -83,6 +79,7 @@ void loop() {
 
   if (eventManager.getTimeKeeper(0)->isTimeUp()) {
     // DEBUG_SERIAL_PRINTLN("Time to read sensors");
+    SensorManager::SensorData sensorData;
     sensorManager.readSensorData(&sensorData);
     model.update(sensorData.temperature, sensorData.humidity, sensorData.pressure);
     eventManager.getTimeKeeper(0)->reset();
@@ -111,6 +108,7 @@ void loop() {
 }
 
 void scan(TwoWire& wire, Adafruit_SSD1306& display) {
+  DEBUG_SERIAL_PRINTLN("SCANNING");
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
@@ -118,13 +116,16 @@ void scan(TwoWire& wire, Adafruit_SSD1306& display) {
   display.print("SCANNING");
   display.display();
   display.setCursor(0, 16);
-  DEBUG_SERIAL_PRINTLN("SCANNING");
 
   uint8_t deviceCount = 0;
   for (uint8_t address = 1; address < 127; address++) {
     wire.beginTransmission(address);
     uint8_t error = wire.endTransmission();
     if (error == 0) {
+      if (address < 16) DEBUG_SERIAL_PRINT("0");
+      DEBUG_SERIAL_PRINTHEX(address);
+      DEBUG_SERIAL_PRINT(" ");
+
       if (deviceCount == 0) {
         display.clearDisplay();
         display.setCursor(0, 0);
@@ -132,10 +133,6 @@ void scan(TwoWire& wire, Adafruit_SSD1306& display) {
       display.printf("%X ", address);
       display.display();
       deviceCount++;
-
-      if (address < 16) DEBUG_SERIAL_PRINT("0");
-      DEBUG_SERIAL_PRINTHEX(address);
-      DEBUG_SERIAL_PRINT(" ");
     }
     delay(10);
   }
